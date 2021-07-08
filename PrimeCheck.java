de3dcf90b8d0842efa07b28e31d36ac159e98c18
lakshminarayanav26 <87144400+lakshminarayanav26@users.noreply.github.com>
2021-07-08 23:30:20 +0530

    Add files via upload

---
+++ PrimeCheck.java
@@ -0,0 +1,34 @@
+package LearnOops;
+
+public class PrimeCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int input = 13;
+		
+		int Count = 0;
+		
+		for (int i = 1; i <=input; i++) {
+			
+			if (input % i == 0) {
+				
+				Count++;
+				
+			}
+			
+		}if (Count == 2) {
+			
+			System.out.println("Given number is prime");
+		
+			
+		}else {
+			
+			System.out.println("Given number is not a prime");
+		}
+		
+		
+
+	}
+
+}
